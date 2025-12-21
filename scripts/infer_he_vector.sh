84fd2bf3b1c2d95edcf6e6533bfcb5c5bdfe178e
Ysong-xy <xy15858267503@sjtu.edu.cn>
2025-12-21 17:45:49 +0800

    fix path

--- scripts/infer_he_vector.sh
+++ scripts/infer_he_vector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m f5_tts_lora/infer/infer_cli.py \
-  --config-path ./F5-TTS/exps/F5TTS_v1_Base_vocos_pinyin_Speech_ESD/2025-12-19_22-21-51 \
+  --config-path f5_tts_lora/exps/F5TTS_v1_Base_vocos_pinyin_Speech_ESD/2025-12-19_22-21-51 \
   --config-name finetune_config \
   ++model.arch.lora_alpha=5.0 \
   "++gen.ref_text='妈妈从我手中拿走我歌曲的歌看，边看边赞不绝口。'" \
--- scripts/train_he_vector.sh
+++ scripts/train_he_vector.sh
@@ -7,11 +7,11 @@ accelerate launch -m f5_tts_lora/train/finetune_cli.py \
     model.tokenized=True\
     model.tokenizer=pinyin \
     model.vocoder.is_local=True \
-    model.vocoder.local_path=./src/third_party/vocos \
+    model.vocoder.local_path=f5_tts_lora/src/third_party/vocos \
     ++model.arch.use_lora=True \
     ++model.arch.lora_rank=64 \
     ++model.arch.lora_feature_dim="[11,5]" \
-    ++ckpts.pretrain=exps/F5TTS_v1_Base/model_1250000.safetensors \
+    ++ckpts.pretrain=f5_tts_lora/exps/F5TTS_v1_Base/model_1250000.safetensors \
     ckpts.logger=wandb \
     ckpts.wandb_project=F5TTS_v1_LoRA \
     ckpts.save_per_updates=10000 \
